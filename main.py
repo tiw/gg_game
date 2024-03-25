5f1ae2a53c5374bed4dc255f7b050d10eff906e1
ting <tting.wang@gmail.com>
2024-03-25 15:05:45 +0800

    Add laser and move laser to the enemy.

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 import pgzrun
 import time
+import math
+from pygame.math import Vector2
 
 # Game settings
 WIDTH = 1024
 HEIGHT = 512
 INIT_BLOOD = 5
 
+
+class GameState():
+    lasers = []
+
+game = GameState()
+
 # Actors
 class Player(Actor):
-    def __init__(self, image, position):
+    def __init__(self, image, position, laser_image, fire_image):
         super().__init__(image)
         self.blood = INIT_BLOOD
         self.hearts = []
         self.pos = position
+        self.fire_image = fire_image
+        self.laser_image = laser_image
 
     def draw_hearts(self, start_pos, step=15, heart_type='emote_heart'):
         for i in range(self.blood):
@@ -26,13 +36,28 @@ class Player(Actor):
         if self.blood > 0:
             self.blood -= damage
 
-player_a = Player('p1_front', (100, 400))
-player_b = Player('p2_front', (924, 400))
+    def fire(self):
+        self.image = self.fire_image
+        laser_pos = (self.pos[0] + 50, self.pos[1])
+        print(laser_pos)
+        ang = self.ang
+        laser = Actor(self.laser_image, laser_pos)
+        laser.angle = -90
+        laser.exact_pos = laser.start_pos = Vector2(self.pos)
+        laser.velocity = Vector2(math.cos(ang), math.sin(ang)).normalize() * 100.0
+        return laser
+
+player_a = Player('p1_front', (100, 400), 'p1_laser', 'p1_jump')
+player_a.ang = 0 
+player_b = Player('p2_front', (924, 400), 'p2_laser', 'p2_jump')
+player_b.ang = 180
 
 # Key handling
 def on_key_down(key):
     if key == keys.K_1:
-        player_a.image = 'p1_jump'  # Ensure this image exists
+        # player_a.image = 'p1_jump'  # Ensure this image exists
+        laser = player_a.fire()
+        game.lasers.append(laser)
         player_b.take_damage()
 
     if key == keys.K_2:  # Placeholder for player B's attack
@@ -43,6 +68,15 @@ def on_key_up(key):
     time.sleep(0.5)
     player_a.image = 'p1_front'  # Reset image after key press
     player_b.image = 'p2_front'
+
+def update(dt):
+    for laser in game.lasers:
+        laser.exact_pos += (laser.velocity * dt)
+        print(laser.exact_pos)
+        print(laser.x)
+        if laser.exact_pos.x < 0 or laser.exact_pos.x > WIDTH:
+            game.lasers.remove(laser)
+        laser.pos = laser.exact_pos.x % WIDTH, laser.exact_pos.y % HEIGHT
 
 # Drawing
 cloud_pos = 200, 100
@@ -57,5 +91,8 @@ def draw():
     player_b.draw()
     player_a.draw_hearts(80)
     player_b.draw_hearts(904)
+
+    for laser in game.lasers:
+        laser.draw()
 
 pgzrun.go()
